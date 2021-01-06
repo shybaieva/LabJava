3d898d9f8f91f7608cd79c6dff9e1bee3dcc5d2a
Tamara13 <tamara13@black-box.zone>
2021-01-06 16:58:23 +0200

    ArrayList

---
+++ src/net/shybaieva/ArrayListGenerics.java
@@ -0,0 +1,205 @@
+package net.shybaieva;
+
+import java.util.NoSuchElementException;
+
+public class ArrayListGenerics <E> {
+    private int capacity;
+    private final int CUT_RANGE = 4;
+
+    private Object[] array;
+
+    private int elementCount;
+
+    public ArrayListGenerics(int initCapacity)
+    {
+        this.capacity = Math.abs(initCapacity);
+        this.array = new Object[this.capacity];
+    }
+
+    public ArrayListGenerics()
+    {
+        this(16);
+    }
+
+    @SuppressWarnings("unchecked")
+    E array(int index)
+    {
+        return (E) array[index];
+    }
+
+    private void resize(int newLength)
+    {
+        Object[] newArray = new Object[newLength];
+        System.arraycopy(array, 0, newArray, 0, elementCount);
+        array = newArray;
+    }
+
+    public int capacity()
+    {
+        return array.length;
+    }
+
+    public int size()
+    {
+        return elementCount;
+    }
+
+    public boolean isEmpty()
+    {
+        return elementCount == 0;
+    }
+
+    public void clear()
+    {
+        if(isEmpty())
+            throw new NoSuchElementException();
+
+        for(int i = elementCount; i > 0; i--)
+            array[i] = null;
+
+        elementCount = 0;
+        resize(capacity);
+    }
+
+    public void print()
+    {
+        for(int i = 0; i < elementCount; i++)
+            System.out.print(array[i] + " ");
+    }
+
+    public boolean contains(E element)
+    {
+        if(isEmpty())
+            throw new NoSuchElementException();
+
+        for(int i = 0; i < elementCount; i++)
+            if(array[i] == element)
+                return true;
+
+        return false;
+    }
+
+    public E set(int index, E element)
+    {
+        if(isEmpty())
+            throw new NoSuchElementException();
+
+        if(index < 0 || index > elementCount - 1)
+            throw new IndexOutOfBoundsException();
+
+        E oldItem = array(index);
+
+        array[index] = element;
+
+        return oldItem;
+    }
+
+    public E get(int index)
+    {
+        if(isEmpty())
+            throw new NoSuchElementException();
+
+        if(index < 0 || index > elementCount - 1)
+            throw new IndexOutOfBoundsException();
+
+        return array(index);
+    }
+
+    public int indexOf(E element)
+    {
+        if(isEmpty())
+            throw new NoSuchElementException();
+
+        for(int i = 0; i < elementCount; i++)
+            if(array[i] == element)
+                return i;
+
+        return -1;
+    }
+
+    public int lastIndexOf(E element)
+    {
+        if(isEmpty())
+            throw new NoSuchElementException();
+
+        if(contains(element))
+        {
+            int lastIndexOf = -1;
+
+            for(int i = 0; i < elementCount; i++)
+                if (array[i] == element)
+                    lastIndexOf = i;
+
+            return lastIndexOf;
+        }
+
+        return -1;
+    }
+
+    public boolean add(E element)
+    {
+        if(elementCount == array.length - 1)
+            resize(array.length * 2);
+
+        array[elementCount] = element;
+
+        elementCount++;
+
+        return true;
+    }
+
+    public void add(int index, E element)
+    {
+        if(elementCount == array.length - 1)
+            resize(array.length * 2);
+
+        System.arraycopy(array, index, array, index + 1,  elementCount - index);
+
+        array[index] = element;
+
+        elementCount++;
+    }
+
+    public E remove(int index)
+    {
+        if(isEmpty())
+            throw new NoSuchElementException();
+
+        if(index < 0 || index > elementCount - 1)
+            throw new IndexOutOfBoundsException();
+
+        E oldItem = array(index);
+
+        if (elementCount - index >= 0) System.arraycopy(array, index + 1, array, index, elementCount - index);
+
+        array[elementCount] = null;
+        elementCount--;
+
+        if(array.length > capacity && elementCount < array.length / CUT_RANGE)
+            resize(array.length/2);
+
+        return oldItem;
+    }
+
+    public boolean remove(E element)
+    {
+        if(isEmpty())
+            throw new NoSuchElementException();
+
+        for(int index = 0; index < elementCount; index++)
+            if(array[index] == element)
+            {
+                System.arraycopy(array, index + 1, array, index, elementCount - index);
+
+                array[elementCount] = null;
+                elementCount--;
+
+                if(array.length > capacity && elementCount < array.length / CUT_RANGE)
+                    resize(array.length/2);
+
+                return true;
+            }
+
+        return false;
+    }
+}
---
+++ src/net/shybaieva/Main.java
@@ -0,0 +1,80 @@
+package net.shybaieva;
+
+import java.util.LinkedList;
+import java.util.Random;
+
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        ArrayListGenerics<String> list = new ArrayListGenerics<>();
+
+        list.add("Hello");
+        list.add("Name");
+        list.add("Surname");
+        list.add(1, "Aoao");
+        list.add("Item");
+
+        list.print();
+        System.out.println("\nSize: " + list.size());
+        System.out.println("Capacity: " + list.capacity());
+        System.out.println("isEmpty: " + list.isEmpty());
+
+        list.remove(0);
+        list.remove("Surname");
+
+        System.out.println();
+
+        list.print();
+        System.out.println("\nSize: " + list.size());
+        System.out.println("Capacity: " + list.capacity());
+        System.out.println("isEmpty: " + list.isEmpty());
+
+        System.out.println("list.contains(\"Name\"): " + list.contains("Name"));
+        System.out.println("list.get(1): " + list.get(1));
+        list.set(2, "NewItem");
+        list.add("Qwerty");
+
+        System.out.println();
+
+        list.print();
+        System.out.println("\nSize = " + list.size());
+        System.out.println("Capacity = " + list.capacity());
+        System.out.println("isEmpty = " + list.isEmpty());
+
+        list.add(0, "1");
+        list.add("1");
+        list.add("2");
+        list.add("2");
+
+        list.add("item0");
+        list.add("item1");
+        list.add("item2");
+        list.add("item3");
+        list.add("item4");
+        list.add("item5");
+        list.add("item6");
+        list.add("item7");
+        list.add("item8");
+        list.add("item9");
+        list.add("item10");
+
+        System.out.println("\nlist.indexOf(\"1\"): " + list.indexOf("1"));
+        System.out.println("list.lastIndexOf(\"1\"): " + list.lastIndexOf("1"));
+
+        System.out.println();
+
+        list.print();
+        System.out.println("\nSize: " + list.size());
+        System.out.println("Capacity: " + list.capacity());
+        System.out.println("isEmpty: " + list.isEmpty());
+
+        System.out.println("\nlist.clear();");
+        list.clear();
+
+        System.out.println("\nSize: " + list.size());
+        System.out.println("Capacity: " + list.capacity());
+        System.out.println("isEmpty: " + list.isEmpty());
+    }
+}
